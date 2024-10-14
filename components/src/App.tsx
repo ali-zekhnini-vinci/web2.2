bb9fb2ff5e6946e13ee98d7613fce4d42f4bed1f
Ali Zekhnini <ali.zekhnini@student.vinci.be>
2024-10-14 17:00:49 +0200

    IHM React Start

---
+++ components/src/App.tsx
@@ -0,0 +1,54 @@
+
+
+function App() {
+  return (
+    <div className="page">
+      <Header title="We Love Pizza" version={0+1} ></Header>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+interface HeaderProps {
+  title: string;
+  version: number;
+}
+
+const Header = (props: HeaderProps) => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+      <h4>Version: {props.version}</h4>
+    </header>
+  );
+};
+
+const Main = () => {
+  return (
+    <main>
+      <p>My HomePage</p>
+      <p>
+        Because we love JS, you can also click on the header to stop / start the
+        music ; )
+      </p>
+      <audio id="audioPlayer" controls autoPlay>
+        <source type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </main>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 className="animate__animated animate__bounce animate__delay-2s">
+        But we also love JS
+      </h1>
+      <img alt="" />
+    </footer>
+  );
+};
+
+export default App;
