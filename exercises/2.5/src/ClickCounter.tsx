087cb9602124184562b6a707b8b912a2588ba313
Ali Zekhnini <ali.zekhnini@student.vinci.be>
2024-11-13 21:54:14 +0100

    new:ex2.5-destructuring

--- exercises/2.5/src/ClickCounter.tsx
+++ exercises/2.5/src/ClickCounter.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const ClickCounter: React.FC = () => {
+interface HeaderProps {
+    title: string;
+    message: string;
+}
+
+const ClickCounter = ({title, message}: HeaderProps) => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <h1>{title}</h1>
       <p>Vous avez cliqué {count} fois</p>
       <button onClick={() => setCount(count + 1)}>Cliquez ici</button>
+      <h4>
+        {count >= 10 ? `${message}`: null}
+      </h4>
     </div>
   );
 };
