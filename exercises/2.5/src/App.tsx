546eb4bf59fef1d44a0f075dbdc98bf777c2dc57
Ali Zekhnini <ali.zekhnini@student.vinci.be>
2024-11-13 22:07:45 +0100

    new:ex2.5-events

--- exercises/2.5/src/App.tsx
+++ exercises/2.5/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <ClickCounter title={'CLICKS'} message={'You are a master in the art of clicking !'} />
+      <ClickCounter title={'CLICKS'} message={'You are a master in the art of clicking !'} messageMouse={'Please click on me now !'} />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- exercises/2.5/src/ClickCounter.tsx
+++ exercises/2.5/src/ClickCounter.tsx
@@ -3,16 +3,25 @@ import { useState } from 'react';
 interface HeaderProps {
     title: string;
     message: string;
+    messageMouse: string;
 }
 
-const ClickCounter = ({title, message}: HeaderProps) => {
+const ClickCounter = ({title, message, messageMouse}: HeaderProps) => {
   const [count, setCount] = useState(0);
+  const [mouseOver, setMouse] = useState(false);
+
+  const handleMouse = () => {
+    setMouse(!mouseOver);
+  }
 
   return (
-    <div>
+    <div >
       <h1>{title}</h1>
       <p>Vous avez cliqué {count} fois</p>
-      <button onClick={() => setCount(count + 1)}>Cliquez ici</button>
+      <h2>
+        {mouseOver ? `${messageMouse}`: ''}
+      </h2>
+      <button onMouseEnter={handleMouse} onMouseLeave={handleMouse} onClick={() => setCount(count + 1)}>Cliquez ici</button>
       <h4>
         {count >= 10 ? `${message}`: null}
       </h4>
